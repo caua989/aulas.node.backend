e6338fbb641f65d450c6de5b4936cc87356c21a0
caua <lopesrookerc@gmail.com>
2026-07-08 20:43:40 -0300

    matematica

--- index.js
+++ index.js
@@ -1,59 +1,39 @@
 import express from "express";
-import bancoDeDados from "./repository/index.js";
 
 const app = express();
-app.get("/api/pessoa/:id", (req, res) => {
-    const id = req.params.id
 
-    const pessoa = bancoDeDados.find(it => it.id == id)
-    if(!pessoa ) {
-        res.send({ message: "pessoa não encontrada"})
-        return
-    }
+app.get("/api/v1/somar", (req, res) => {
+    const { num1, num2 } = req.query
 
-    res.send({ pessoa })
-})
-app.get("/api/pessoa", (req, res) => {
-    const { id, name } = req.query
-
-    if(!id || !name) {
-        res.send({ message: "Favor informar id e name"})
-        return
-    }
-
-    bancoDeDados.push({ id, name })
-    //console.log(bancoDeDados)
-
-     res.send({ message: "Pessoa criada com sucesso" })
+    const resultado = Number(num1) + Number(num2)
+    
+    res.send ({ message: resultado})
 })
 
-app.get("/api/deletar/:id", (req, res) => {
-    const id = req.query.id
+app.get("/api/v1/subtrair", (req, res) => {
+     const { num1, num2 } = req.query
 
-    if(!id) {
-        res.send({ message: "Pessoa não encontrada"})
-        return
-    }
+     const resultado = Number(num1) - Number(num2)
+    
+     res.send ({ message: resultado})
+     })
 
-    bancoDeDados.delete({ id, name })
+ app.get("/api/v1/multiplicacao", (req, res) => {
+     const { num1, num2 } = req.query
 
-    res.send({ message: "Pessoa deletada com sucesso" })
-})
+     const resultado = Number(num1) * Number(num2)
+    
+     res.send ({ message: resultado})
+ })
+
+ app.get("/api/v1/divisao", (req, res) => {
+     const { num1, num2 } = req.query
+
+     const resultado = Number(num1) / Number(num2)
+    
+     res.send ({ message: resultado})
+ })
 
 app.listen(3000, () => {
-    console.log("Servidor escutando na porta 3000");
-}) // http://localhost:3000/api/pessoa/abcde
-
-
-
-
-//app.get("/api/v1/somar", (req, res) => {
-    //console.log(req.query)
-    
-    //const num1 = Number(req.query.batata);
-    //const num2 = Number(req.query.num2);
-
-    //const resultado = num1 + num2;
-
-    //res.send({ resultado });
-//})
+    console.log("");
+})
