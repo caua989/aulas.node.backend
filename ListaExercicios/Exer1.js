30d9115e524034f4f7d9de4f0c255d589ccb139c
caua <lopesrookerc@gmail.com>
2026-07-16 20:37:44 -0300

    1?

--- ListaExercicios/Exer1.js
+++ ListaExercicios/Exer1.js
@@ -2,8 +2,14 @@ import express from "express";
 
 const app = express();
 
-app.post("/api/v1/somar", (req, res) => {
-    const { produto, custo } = req.body
-    
-    res.status(200).send ({ message: produto + custo})
-})
+app.get("/api/exer1", (req, res) => {
+    const { produto, custo } = req.query;
+
+    res.status(200).send({
+        message: produto + custo
+    });
+});
+
+app.listen(3000, () => {
+    console.log("Servidor rodando na porta 3000");
+});
