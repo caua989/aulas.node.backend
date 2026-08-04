c3006373ef05fc35ada630cbefe72a799c65e559
caua <lopesrookerc@gmail.com>
2026-08-03 21:32:42 -0300

    metade aula 5

--- index.js
+++ index.js
@@ -7,38 +7,6 @@ app.use(express.json())
 
 app.use("/api/v1/cachorro", cachorro)
 
-// app.post("/api/v1/somar", (req, res) => {
-//     const { num1, num2 } = req.body
-
-//     const resultado = Number(num1) + Number(num2)
-    
-//     res.status(200).send ({ message: resultado})
-// })
-
-// app.post("/api/v1/subtrair", (req, res) => {
-//      const { num1, num2 } = req.body
-
-//      const resultado = Number(num1) - Number(num2)
-    
-//      res.send ({ message: resultado})
-//      })
-
-//  app.get("/api/v1/multiplicar", (req, res) => {
-//      const { num1, num2 } = req.query
-
-//      const resultado = Number(num1) * Number(num2)
-    
-//      res.send ({ message: resultado})
-//  })
-
-//  app.get("/api/v1/dividir", (req, res) => {
-//      const { num1, num2 } = req.query
-
-//      const resultado = Number(num1) / Number(num2)
-    
-//      res.send ({ message: resultado})
-//  })
-
 app.listen(3000, () => {
     console.log("executando na porta 3000");
 })
