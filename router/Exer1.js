ac2c9c957fd6e5df669af5301f1fe0a879dd3bd4
caua <lopesrookerc@gmail.com>
2026-08-17 19:17:19 -0300

    1

--- router/Exer1.js
+++ router/Exer1.js
@@ -1,13 +1,15 @@
-import express from "express";
-import exer1 from "../repository/exer1.js";
+import express from "express"
+import exer1 from "../repository/exer1.js"
 
 const router = express.Router()
 
-router.get("/exer1", (req, res) => {
-    const { produto, custo } = req.query;
+router.post("/exer1/criar", (req, res) => {
+    const { produto, custo } = req.query
+
+    if(!produto, custo)
 
     res.status(200).send({
-        message: produto + custo});
+        message: produto + custo})
 });
 
 export default router
